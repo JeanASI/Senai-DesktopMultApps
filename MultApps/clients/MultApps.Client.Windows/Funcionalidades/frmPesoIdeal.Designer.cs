c659cdababa90d7f6b2e28a1a4fd243c3b3bc925
JeanASI <jeanderson.a.silva@aluno.senai.br>
2022-09-08 21:54:23 -0300

    Atualização do formulario Peso Ideal

--- MultApps/clients/MultApps.Client.Windows/Funcionalidades/frmPesoIdeal.Designer.cs
+++ MultApps/clients/MultApps.Client.Windows/Funcionalidades/frmPesoIdeal.Designer.cs
@@ -28,88 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.rbnFeminino = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.gbxSexo = new System.Windows.Forms.GroupBox();
+            this.gbxSexo.SuspendLayout();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // rbnMasculino
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.radioButton1.Location = new System.Drawing.Point(23, 59);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(104, 25);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Masculino";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbnMasculino.Location = new System.Drawing.Point(15, 21);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(80, 19);
+            this.rbnMasculino.TabIndex = 0;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            this.rbnMasculino.CheckedChanged += new System.EventHandler(this.rbnMasculino_CheckedChanged);
             // 
-            // radioButton2
+            // rbnFeminino
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.radioButton2.Location = new System.Drawing.Point(144, 59);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(98, 25);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Feminino";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(23, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 21);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Sexo";
+            this.rbnFeminino.AutoSize = true;
+            this.rbnFeminino.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbnFeminino.Location = new System.Drawing.Point(124, 21);
+            this.rbnFeminino.Name = "rbnFeminino";
+            this.rbnFeminino.Size = new System.Drawing.Size(75, 19);
+            this.rbnFeminino.TabIndex = 1;
+            this.rbnFeminino.TabStop = true;
+            this.rbnFeminino.Text = "Feminino";
+            this.rbnFeminino.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(291, 33);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(239, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 21);
+            this.label2.Size = new System.Drawing.Size(45, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Altura :";
             // 
-            // label3
+            // lblPesoIdeal
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(158, 164);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 21);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Peso Ideal";
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPesoIdeal.Location = new System.Drawing.Point(127, 77);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(60, 15);
+            this.lblPesoIdeal.TabIndex = 4;
+            this.lblPesoIdeal.Text = "Peso Ideal";
             // 
-            // textBox1
+            // txtAltura
             // 
-            this.textBox1.Location = new System.Drawing.Point(291, 63);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 23);
-            this.textBox1.TabIndex = 5;
+            this.txtAltura.Location = new System.Drawing.Point(239, 41);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(87, 23);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // gbxSexo
+            // 
+            this.gbxSexo.Controls.Add(this.rbnMasculino);
+            this.gbxSexo.Controls.Add(this.rbnFeminino);
+            this.gbxSexo.Location = new System.Drawing.Point(12, 12);
+            this.gbxSexo.Name = "gbxSexo";
+            this.gbxSexo.Size = new System.Drawing.Size(212, 52);
+            this.gbxSexo.TabIndex = 6;
+            this.gbxSexo.TabStop = false;
+            this.gbxSexo.Text = "Sexo";
             // 
             // frmPesoIdeal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(335, 165);
+            this.Controls.Add(this.gbxSexo);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblPesoIdeal);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Name = "frmPesoIdeal";
             this.Text = "frmPesoIdeal";
+            this.gbxSexo.ResumeLayout(false);
+            this.gbxSexo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,11 +120,11 @@
 
         #endregion
 
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private Label label1;
+        private RadioButton rbnMasculino;
+        private RadioButton rbnFeminino;
         private Label label2;
-        private Label label3;
-        private TextBox textBox1;
+        private Label lblPesoIdeal;
+        private TextBox txtAltura;
+        private GroupBox gbxSexo;
     }
 }
